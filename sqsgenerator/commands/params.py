4eb896e46cfa62b9b2de017ac9f1445355bbcb2b
dominik <dominik.noeger@unileoben.ac.at>
2021-08-13 12:30:06 +0200

    :art: use click_settings_file decorator for input file parsing :recycle: moved num_max_output_configurations

--- sqsgenerator/commands/params.py
+++ sqsgenerator/commands/params.py
@@ -1,81 +1,15 @@
-import io
-import pprint
-import sys
 
+import sys
 import click
-import attrdict
-import typing as T
-from sqsgenerator.compat import require, Feature as F, have_feature, get_module
+from sqsgenerator.commands.common import read_settings_file, pretty_print, dumps, click_settings_file
 from sqsgenerator.settings import process_settings, settings_to_dict, parameter_list, construct_settings
 
 
-def error(message, exc_type=click.Abort, raise_exc=True, style=None):
-    style = style or dict(fg='red', bold=True)
-    click.secho(message, **style)
-    if raise_exc: raise exc_type(message)
-
-
-def pretty_print(*objects, **kwargs):
-    buf = io.StringIO()
-    if have_feature(F.rich):
-        get_module(F.rich).print(*objects, **kwargs)
-    else:
-        from pprint import pprint
-        for o in objects: pprint(o, stream=buf, **kwargs)
-    return buf.getvalue()
-
-
-@require(F.json, F.yaml, F.pickle, condition=any)
-def dumps(o: dict, output_format='yaml'):
-    f = F(output_format)
-    if not have_feature(f):
-        error(f'The package "{format}" is not installed, consider to install it with')
-
-    def safe_dumps(d, **kwargs):
-        buf = io.StringIO()
-        get_module(F.yaml).safe_dump(d, buf, **kwargs)
-        return buf.getvalue()
-    dumpers = {
-        F.json: lambda d: get_module(F.json).dumps(d, indent=4),
-        F.pickle: lambda d: get_module(F.pickle).dumps(d),
-        F.yaml: lambda d: safe_dumps(d, default_flow_style=None)
-    }
-    content = dumpers[f](o)
-    return content if f == F.pickle else content.encode()
-
-
-@require(F.yaml, F.json, condition=any)
-def read_settings_file(path, format='yaml') -> T.Optional[attrdict.AttrDict]:
-    f = F(format)
-    readers = {
-        F.json: 'loads',
-        F.pickle: 'loads',
-        F.yaml: 'safe_load'
-    }
-    if not have_feature(f):
-        error(f'The package "{format}" is not installed, consider to install it with')
-    reader = getattr(get_module(f), readers[f])
-    try:
-        with open(path, 'r' if f != F.pickle else 'rb') as settings_file:
-            content = settings_file.read()
-    except FileNotFoundError:
-        error(f'The settings file "{path}" does not exist')
-        return
-    try:
-        data = attrdict.AttrDict(reader(content))
-    except Exception as e:
-        error(f'While reading the file "{path}", a "{type(e).__name__}" occurred. Maybe the file has the wrong format. I was expecting a "{format}"-file. You can specify a different input-file format using the "--format" option')
-        return
-    return data
-
-
 @click.command('show')
-@click.argument('filename', type=click.Path(exists=True))
-@click.option('--input-format', '-if', type=click.Choice(['yaml', 'json', 'pickle']), default='yaml')
 @click.option('--param', '-p', type=click.Choice(parameter_list()), default=None, multiple=True)
 @click.option('--output-format', '-of', type=click.Choice(['yaml', 'json', 'pickle', 'native']), default='native')
-def show(filename, input_format, param, output_format):
-    settings = read_settings_file(filename, format=input_format)
+@click_settings_file('all')
+def show(settings, param, output_format):
     parameters_to_process = set(param or parameter_list())
     processed = process_settings(settings, params=parameters_to_process)
     filtered = {k : v for k, v in settings_to_dict(processed).items() if k in parameters_to_process}
@@ -84,11 +18,9 @@ def show(filename, input_format, param, output_format):
 
 
 @click.command('check')
-@click.argument('filename', type=click.Path(exists=True))
-@click.option('--input-format', '-if', type=click.Choice(['yaml', 'json', 'pickle']), default='yaml')
-def check(filename, input_format):
-    settings = read_settings_file(filename, format=input_format)
-    construct_settings(settings)
+@click_settings_file('all')
+def check(settings):
+    construct_settings(settings, process=False)
 
 
 @click.group('params')
--- sqsgenerator/core/__init__.py
+++ sqsgenerator/core/__init__.py
@@ -2,7 +2,7 @@ import logging
 import typing as T
 from sqsgenerator.core.data import Structure, available_species
 from sqsgenerator.core.iteration import IterationMode, IterationSettings, __version__, __features__, BoostLogLevel, set_log_level
-from sqsgenerator.core.utils import default_shell_distances
+from sqsgenerator.core.utils import default_shell_distances, total_permutations, rank_structure
 
 TRACE = 5
 logging.addLevelName(TRACE, 'TRACE')
@@ -17,6 +17,7 @@ def set_core_log_level(level: int = logging.WARNING) -> T.NoReturn:
         logging.ERROR: BoostLogLevel.error
     }
     set_log_level(level_map.get(level))
+
 
 class BadSettings(Exception):
     pass
