83ef39ce0170d696befa3d0135f58909db4efff6
dominik <dominik.noeger@unileoben.ac.at>
2021-08-24 09:29:23 +0200

    :coffin: remove try_ function (functional.py)

--- sqsgenerator/settings/functional.py
+++ sqsgenerator/settings/functional.py
@@ -57,18 +57,6 @@ def if_(condition):
     return then_
 
 
-def try_(f: T.Callable, *args, exc_type: T.Type = Exception, raise_exc: bool=True, return_success: bool =False, msg: T.Optional[str]=None, log_exc_info: bool=False, **kwargs) -> T.Any:
-    try:
-        result = f(*args, **kwargs)
-        success = True
-    except exc_type as e:
-        if msg: get_function_logger(f).exception(f'Failed to {msg}', exc_info=e if log_exc_info else None)
-        if raise_exc: raise
-        result = None
-        success = False
-    return (success, result) if return_success else result
-
-
 def isa(o: T.Tuple[type]):
     """
     Constructs a type-checking predicate. Represents isinstance(x, o)
