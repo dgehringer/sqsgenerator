e7ac568c8897e4503719bec4f744fcbd91f79bc8
Dominik Gehringer <dgehringer@protonmail.com>
2021-09-10 12:26:36 +0200

    :white_check_mark: added tests for analyse :coffin: remove old test routines

--- test/cli/test_command_run.py
+++ test/cli/test_command_run.py
@@ -1,9 +1,11 @@
 
 import os
+import yaml
 import unittest
+import numpy as np
 import click.testing
 from cli import inject_config_file
-from sqsgenerator.io import compression_to_file_extension
+from sqsgenerator.io import compression_to_file_extension, read_settings_file
 from sqsgenerator.cli import cli
 
 
@@ -32,6 +34,13 @@ class TestRunIterationCommand(unittest.TestCase):
         self.assertTrue(os.path.exists(result_file))
         self.assertNotEqual(r.exit_code, 0)
 
+        r = self.cli_runner.invoke(cli, ['export'])
+        self.assertTrue(os.path.exists(result_file))
+        self.assertEqual(r.exit_code, 0)
+
+        r = self.cli_runner.invoke(cli, ['export', 'sqs.yaml'])
+        self.assertNotEqual(r.exit_code, 0)
+
     @inject_config_file()
     def test_run_iteration_export_compression(self):
         for compression in compression_to_file_extension.keys():
@@ -46,8 +55,33 @@ class TestRunIterationCommand(unittest.TestCase):
             self.assertTrue(os.path.exists(archive_name))
 
     @inject_config_file()
-    def test_params_check(self):
-        pass
+    def test_analyse_command(self):
+        result_file = 'sqs.result.yaml'
+        r = self.cli_runner.invoke(cli, ['run', 'iteration', '--export', '--no-minimal', '--similar', '--dump-include', 'parameters', '--dump-include', 'objective'])
+        self.assertTrue(os.path.exists(result_file))
+        self.assertEqual(r.exit_code, 0)
+
+        results_from_iteration = read_settings_file(result_file).configurations
+
+        r = self.cli_runner.invoke(cli, ['analyse', 'sqs.yaml'])
+        self.assertNotEqual(r.exit_code, 0)
+
+        r = self.cli_runner.invoke(cli, ['analyse'])
+        self.assertEqual(r.exit_code, 0)
+
+        r = self.cli_runner.invoke(cli, ['analyse', '-of', 'yaml'])
+        self.assertEqual(r.exit_code, 0)
+        results_from_analyse = yaml.safe_load(r.output)['configurations']
+
+        self.assertListEqual(list(results_from_analyse.keys()), list(results_from_iteration.keys()))
+
+        for k in results_from_analyse.keys():
+            iteration, analyse = results_from_iteration[k], results_from_analyse[k]
+            # self.assertAlmostEqual(iteration['objective'], analyse['objective'])
+            self.assertListEqual(iteration['configuration'], analyse['configuration'])
+            np.testing.assert_array_almost_equal(iteration['parameters'], analyse['parameters'])
+
+
 
 
 if __name__ == '__main__':
