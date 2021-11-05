8462a20c1670cd3ba914a0bc5df0dd5c7489c9b6
Dominik Gehringer <dgehringer@protonmail.com>
2021-11-05 12:31:39 +0100

    :art: added __all__ export definition

--- sqsgenerator/__init__.py
+++ sqsgenerator/__init__.py
@@ -2,4 +2,16 @@
 import sqsgenerator.cli
 import sqsgenerator.public
 
-from sqsgenerator.public import pair_sqs_iteration, process_settings, sqs_optimize
+from sqsgenerator.public import process_settings, sqs_optimize, from_pymatgen_structure, from_ase_atoms, \
+    to_ase_atoms, to_pymatgen_structure, IterationMode, Structure
+
+__all__ = [
+    'process_settings',
+    'sqs_optimize',
+    'from_ase_atoms',
+    'from_pymatgen_structure',
+    'to_ase_atoms',
+    'to_pymatgen_structure',
+    'IterationMode',
+    'Structure'
+]
