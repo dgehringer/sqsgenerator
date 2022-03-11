e609a5771d1cfd5a7ad275dc48d249abd71d6a8d
Dominik Gehringer <dgehringer@protonmail.com>
2022-03-11 15:28:38 +0100

    :bookmark: bump version 0.0.5 → 0.1

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ class CMakeBuildExt(build_ext):
         
 setup(
     name='sqsgenerator',
-    version="0.1",
+    version='0.1',
     description='A simple command line Special Quasirandom Structure generator in Python',
     author_email='dgehringer@protonmail.com',
     license='MIT',
