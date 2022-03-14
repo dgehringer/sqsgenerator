d13a3e5963781df33c1a4421e5c67a52e061dfd0
Dominik Gehringer <dgehringer@protonmail.com>
2022-03-14 14:35:28 +0100

    :building_construction: added attrdict fallback

---
+++ sqsgenerator/fallback/__init__.py
@@ -0,0 +1,2 @@
+
+from sqsgenerator.fallback import attrdict
---
+++ sqsgenerator/fallback/attrdict/__init__.py
@@ -0,0 +1,7 @@
+"""
+attrdict contains several mapping objects that allow access to their
+keys as attributes.
+"""
+from sqsgenerator.fallback.attrdict.dictionary import AttrDict
+
+__all__ = ['AttrDict']
