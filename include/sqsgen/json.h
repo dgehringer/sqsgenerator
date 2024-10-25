518a239910ca0d0625c203682246d641930f110d
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-10-25 17:06:54 +0200

    🗃️ register Eigen serializers

--- include/sqsgen/json.h
+++ include/sqsgen/json.h
@@ -10,6 +10,35 @@
 #include "core/structure.h"
 #include "types.h"
 
+using namespace sqsgen;
+
+// partial specialization (full specialization works too)
+NLOHMANN_JSON_NAMESPACE_BEGIN
+template <class T> struct adl_serializer<matrix_t<T>> {
+  static void to_json(json& j, const matrix_t<T>& m) { j = core::helpers::eigen_to_stl(m); }
+
+  static void from_json(const json& j, matrix_t<T>& m) {
+    m = std::move(core::helpers::stl_to_eigen<matrix_t<T>>(j.get<stl_matrix_t<T>>()));
+  }
+};
+
+template <class T> struct adl_serializer<lattice_t<T>> {
+  static void to_json(json& j, const lattice_t<T>& m) { j = core::helpers::eigen_to_stl(m); }
+
+  static void from_json(const json& j, lattice_t<T>& m) {
+    m = std::move(core::helpers::stl_to_eigen<lattice_t<T>>(j.get<stl_matrix_t<T>>()));
+  }
+};
+
+template <class T> struct adl_serializer<coords_t<T>> {
+  static void to_json(json& j, const coords_t<T>& m) { j = core::helpers::eigen_to_stl(m); }
+
+  static void from_json(const json& j, coords_t<T>& m) {
+    m = std::move(core::helpers::stl_to_eigen<coords_t<T>>(j.get<stl_matrix_t<T>>()));
+  }
+};
+NLOHMANN_JSON_NAMESPACE_END
+
 namespace sqsgen {
   using json = nlohmann::json;
 
