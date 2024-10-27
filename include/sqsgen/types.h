6b7936972f0673d08ce0e5bb6bedaff1503a125e
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-10-27 10:53:36 +0100

    ♻️ factor out templates

--- include/sqsgen/types.h
+++ include/sqsgen/types.h
@@ -11,6 +11,7 @@
 #include <vector>
 
 namespace sqsgen {
+
   using specie_t = uint8_t;
   using rank_t = mppp::integer<2>;
   using configuration_t = std::vector<specie_t>;
@@ -25,13 +26,19 @@ namespace sqsgen {
     requires std::is_arithmetic_v<T>
   using coords_t = Eigen::Matrix<T, Eigen::Dynamic, 3>;
 
+  template <class T> using matrix_t = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+  template <class T> using stl_matrix_t = std::vector<std::vector<T>>;
+
+  using shell_matrix_t = matrix_t<std::size_t>;
+
   template <class T>
-  using matrix_t = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+    requires std::is_arithmetic_v<T>
+  using shell_weights_t = std::map<std::size_t, T>;
 
-  template<class T>
-  using stl_matrix_t = std::vector<std::vector<T>>;
+  using index_type_list = std::tuple<std::uint_fast8_t, std::uint_fast16_t, std::uint_fast32_t>;
 
-  using shell_matrix_t = std::variant<matrix_t<std::uint_fast8_t>, matrix_t<std::uint_fast16_t>, matrix_t<std::uint_fast32_t>, matrix_t<std::uint_fast64_t>>;
+
 
 }  // namespace sqsgen
 #endif  // SQSGEN_TYPES_HPP
