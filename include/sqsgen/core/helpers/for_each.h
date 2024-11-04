a3d38df17a8a079f569659139b7be8558b83f82d
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-11-04 22:49:04 +0100

    ✨ add enumerate view

--- include/sqsgen/core/helpers/for_each.h
+++ include/sqsgen/core/helpers/for_each.h
@@ -24,6 +24,10 @@ namespace sqsgen::core::helpers {
 
   template <ranges::input_range R> constexpr auto range(R&& r) { return std::forward<R>(r); }
 
+  template <ranges::input_range R> constexpr auto enumerate(R&& r) {
+    return views::zip(views::iota(0), r);
+  }
+
   template <class Fn, std::size_t I = 0, class... Args> void for_each(Fn&& fn, Args&&... args) {
     static_assert(I < sizeof...(Args), "For each must be at least one element");
     auto arg = std::get<I>(std::forward_as_tuple(std::forward<Args>(args)...));
@@ -38,6 +42,6 @@ namespace sqsgen::core::helpers {
       }
     }
   }
-}
+}  // namespace sqsgen::core::helpers
 
-#endif //SQSGEN_CORE_HELPERS_FOR_EACH_H
+#endif  // SQSGEN_CORE_HELPERS_FOR_EACH_H
