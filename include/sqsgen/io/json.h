5687a222e32a98c7bfc05fd4eb0fc2f953bd5e73
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-11-17 21:14:36 +0100

    ✨ json parser helpers

--- include/sqsgen/io/json.h
+++ include/sqsgen/io/json.h
@@ -7,14 +7,13 @@
 
 #include <nlohmann/json.hpp>
 
-#include "sqsgen/config.h"
 #include "sqsgen/core/structure.h"
 #include "sqsgen/types.h"
 
-using namespace sqsgen;
-
 // partial specialization (full specialization works too)
 NLOHMANN_JSON_NAMESPACE_BEGIN
+
+using namespace sqsgen;
 template <class T> struct adl_serializer<matrix_t<T>> {
   static void to_json(json& j, const matrix_t<T>& m) { j = core::helpers::eigen_to_stl(m); }
 
@@ -68,41 +67,46 @@ NLOHMANN_JSON_NAMESPACE_END
 
 namespace sqsgen {
 
-  using json = nlohmann::json;
-
   NLOHMANN_JSON_SERIALIZE_ENUM(Prec, {{PREC_SINGLE, "single"}, {PREC_DOUBLE, "double"}})
 
-  struct parse_error {
-    std::string parameter;
-    std::string msg;
+  enum parse_error_code { CODE_UNKNOWN = -1, CODE_NOT_FOUND = 0, CODE_TYPE_ERROR = 1 };
 
-    template <core::helpers::string_literal key>
-    static parse_error from_msg(const std::string& msg) {
-      return parse_error{key.data, msg};
+  struct parse_error {
+    std::string key;
+    std::string msg;
+    parse_error_code code;
+    std::optional<std::string> parameter = std::nullopt;
+
+    template <core::helpers::string_literal key, parse_error_code code>
+    static parse_error from_msg(const std::string& msg,
+                                std::optional<std::string> parameter = std::nullopt) {
+      return parse_error{key.data, msg, code, parameter};
     }
   };
 
-  template <core::helpers::string_literal key, class T>
-  std::optional<T> get_optional(json const& json) {
-    if (json.contains(key.data)) {
-      return std::make_optional(json.at(key.data).template get<T>());
-    }
-    return std::nullopt;
+  template <class... Args> using parse_result_t = std::variant<parse_error, Args...>;
+
+  template <class... Args> bool holds_result(parse_result_t<Args...> const& a) {
+    return !std::holds_alternative<parse_error>(a);
+  }
+
+  template <class... Args> bool holds_error(parse_result_t<Args...> const& a) {
+    return std::holds_alternative<parse_error>(a);
   }
 
   template <core::helpers::string_literal key, class Option>
-  std::variant<parse_error, Option> get_as_helper(nlohmann::json const& json) {
+  parse_result_t<Option> get_as(nlohmann::json const& json) {
     try {
       return json.at(key.data).template get<Option>();
     } catch (nlohmann::json::out_of_range const&) {
-      return parse_error::from_msg<key>("out of range");
+      return parse_error::from_msg<key, CODE_TYPE_ERROR>("out of range");
     } catch (nlohmann::json::type_error const&) {
-      return parse_error::from_msg<key>("type error");
+      return parse_error::from_msg<key, CODE_TYPE_ERROR>("type error");
     }
   };
 
-  template <class ... Options, class Option>
-  std::variant<parse_error, Options ...> forward_variant(std::variant<parse_error, Option> && opt) {
+  template <class... Options, class Option>
+  parse_result_t<Options...> forward_variant(std::variant<parse_error, Option>&& opt) {
     if (std::holds_alternative<parse_error>(opt)) {
       return std::get<parse_error>(opt);
     } else {
@@ -111,15 +115,41 @@ namespace sqsgen {
   }
 
   template <core::helpers::string_literal key, class... Options>
-  std::optional<std::variant<parse_error, Options...>> get_either(nlohmann::json const& json) {
+  std::optional<parse_result_t<Options...>> get_either_optional(nlohmann::json const& json) {
     if (json.contains(key.data)) {
-      std::variant<parse_error, Options...> result = parse_error::from_msg<key>(std::format("failed to load {}", key.data));
-      ((result = forward_variant<Options...>(get_as_helper<key, Options>(json))), ...);
+      std::variant<parse_error, Options...> result
+          = parse_error::from_msg<key, CODE_UNKNOWN>(std::format("failed to load {}", key.data));
+      ((result = forward_variant<Options...>(get_as<key, Options>(json))), ...);
       return result;
     }
     return std::nullopt;
   }
 
+  template <core::helpers::string_literal key, class... Options>
+  parse_result_t<Options...> get_either(nlohmann::json const& json) {
+    return get_either_optional<key, Options...>(json).value_or(
+        parse_error::from_msg<key, CODE_NOT_FOUND>("could not find key {}", key.data));
+  }
+
+  template <core::helpers::string_literal key, class Value>
+  std::optional<parse_result_t<Value>> get_optional(nlohmann::json const& json) {
+    return get_either_optional<key, Value>(json);
+  }
+
+  template <class... Args>
+  parse_result_t<std::tuple<Args...>> combine(parse_result_t<Args> const& ...args) {
+    std::optional<parse_error> error = std::nullopt;
+    ((error = error.has_value()
+                  ? error
+                  : (holds_error<Args>(args) ? std::get<parse_error>(args) : std::nullopt)),
+     ...);
+    if (error.has_value()) {
+      return error.value();
+    } else {
+      return std::make_tuple(std::get<Args>(args)...);
+    }
+
+  }
 }  // namespace sqsgen
 
 #endif  // SQSGEN_IO_JSON_H
