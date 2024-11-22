949909baad56fe5ee59731511127736346ee949b
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-11-22 15:13:48 +0100

    ♻️ add helper functions

--- tests/helpers.h
+++ tests/helpers.h
@@ -7,6 +7,8 @@
 
 #include <gtest/gtest.h>
 
+#include "core/structure.h"
+
 namespace sqsgen::testing::helpers {
   template <class Matrix> void assert_matrix_equal(const Matrix& lhs, const Matrix& rhs,
                                                    typename Matrix::Scalar epsilon = 1.0e-7) {
@@ -17,6 +19,21 @@ namespace sqsgen::testing::helpers {
         ASSERT_NEAR(lhs(row, col), rhs(row, col), epsilon)
             << std::format("Error at position ({},{})", row, col);
       }
+    }
+  }
+
+  template <class T>
+  void assert_structure_equal(core::structure<T> const& lhs, core::structure<T> const& rhs, T epsilon = 1.0e-7) {
+    ASSERT_EQ(lhs.size(), rhs.size()) << "Size of lhs and rhs are not equal";
+    auto lhs_sites = core::helpers::as<std::vector>{}(lhs.sites());
+    auto rhs_sites = core::helpers::as<std::vector>{}(rhs.sites());
+    for (int i = 0; i < lhs.size(); ++i) {
+      auto lsite = lhs_sites[i];
+      auto rsite = rhs_sites[i];
+      ASSERT_EQ(lsite.specie, rsite.specie);
+      ASSERT_NEAR(lsite.frac_coords(0), rsite.frac_coords(0), epsilon);
+      ASSERT_NEAR(lsite.frac_coords(1), rsite.frac_coords(1), epsilon);
+      ASSERT_NEAR(lsite.frac_coords(2), rsite.frac_coords(2), epsilon);
     }
   }
 
