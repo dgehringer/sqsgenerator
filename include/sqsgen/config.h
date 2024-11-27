f33072527d25c1dd635555809ad8ef6e02fd5559
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-11-27 21:44:59 +0100

    💬 add default values

--- include/sqsgen/config.h
+++ include/sqsgen/config.h
@@ -8,12 +8,14 @@
 #include <optional>
 
 #include "io/json.h"
-#include "sqsgen/io/parser/structure.h"
+#include "sqsgen/io/parser/structure_config.h"
 
 namespace sqsgen {
 
-  template <class T> class structure_configuration {
+  template <class T> class structure_config {
   public:
+    static constexpr auto DEFAULT_SUPERCELL = std::array{1,1,1};
+    static constexpr auto DEFAULT_WHICH = std::vector<int>{};
     lattice_t<T> lattice;
     coords_t<T> coords;
     configuration_t species;
@@ -23,18 +25,27 @@ namespace sqsgen {
     core::structure<T> structure(bool supercell = true, bool which = true) {
       core::structure<T> structure(lattice, coords, species);
       if (supercell) {
-        auto [a, b, c] = this->supercell.value_or(std::array{1, 1, 1});
+        auto [a, b, c] = this->supercell.value_or(DEFAULT_SUPERCELL);
         structure = structure.supercell(a, b, c);
       }
       if (which) {
-        auto slice = this->which.value_or(std::vector<int>{});
+        auto slice = this->which.value_or(DEFAULT_WHICH);
         if (!slice.empty()) structure = structure.sliced(slice);
       }
       return structure;
     }
   };
 
-  template <class T> struct configuration {};
+  class composition_config {
+    public:
+
+  };
+
+
+
+  template <class T> struct configuration {
+    structure_config<T> structure;
+  };
 
 }  // namespace sqsgen
 
