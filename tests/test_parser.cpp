c4dcd3d4585d0852d9fed7f9b2f357952370a7a7
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-11-30 17:21:06 +0100

    ✅ simplify test code

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -5,8 +5,8 @@
 #include <gtest/gtest.h>
 
 #include "helpers.h"
-#include "sqsgen/config.h"
 #include "nlohmann/json.hpp"
+#include "sqsgen/config.h"
 #include "sqsgen/core/structure.h"
 #include "sqsgen/io/config/structure.h"
 #include "sqsgen/types.h"
@@ -15,13 +15,14 @@ namespace sqsgen::testing {
   using json = nlohmann::json;
   using namespace sqsgen::io::config;
 
-  template <core::helpers::string_literal key, parse_error_code code, class... Args>
-  void assert_holds_error(nlohmann::json const& a) {
-    auto r = io::config::parse_structure_config<double>(a);
-    ASSERT_TRUE(holds_error(r));
-    parse_error error = std::get<parse_error>(r);
-    ASSERT_EQ(key.data, error.key);
-    ASSERT_EQ(code, error.code) << error.msg;
+  template <class Fn> auto make_assert_holds_error(Fn&& fn) {
+    return [&](std::string const& key, parse_error_code code, nlohmann::json const& a) {
+      auto r = fn(a);
+      ASSERT_TRUE(holds_error(r));
+      parse_error error = std::get<parse_error>(r);
+      ASSERT_EQ(key, error.key);
+      ASSERT_EQ(code, error.code) << error.msg;
+    };
   }
 
   template <class T> const static auto TEST_FCC_STRUCTURE = core::structure<T>{
@@ -29,71 +30,73 @@ namespace sqsgen::testing {
       coords_t<T>{{0.0, 0.0, 0.0}, {0.0, 0.5, 0.5}, {0.5, 0.0, 0.5}, {0.5, 0.5, 0.0}},
       std::vector<specie_t>{11, 12, 13, 14}};
 
-  TEST(test_parse_structure, empty) { ASSERT_TRUE(holds_error(io::config::parse_structure_config<double>({}))); }
+  TEST(test_parse_structure, empty) {
+    ASSERT_TRUE(holds_error(io::config::parse_structure_config<"composition", double>({})));
+  }
 
   TEST(test_parse_structure, required_fields_success) {
     auto s = TEST_FCC_STRUCTURE<double>;
-    auto r1 = io::config::parse_structure_config<double>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", s.species},
-    });
+    json document = {{"structure",
+                  {
+                            {"lattice", s.lattice},
+                            {"coords", s.frac_coords},
+                            {"species", s.species},
+                        }}};
+
+    auto r1 = io::config::parse_structure_config<"structure", double>(document);
+
+
     ASSERT_TRUE(holds_result(r1));
     helpers::assert_structure_equal(s, get_result(r1).structure());
 
-    auto r2 = io::config::parse_structure_config<double>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", std::vector<std::string>{"Na", "Mg", "Al", "Si"}},
-    });
+    document["structure"]["species"] = std::vector<std::string>{"Na", "Mg", "Al", "Si"};
+    auto r2 = io::config::parse_structure_config<"structure", double>(document);
     ASSERT_TRUE(holds_result(r2));
     helpers::assert_structure_equal(s, get_result(r2).structure());
   }
 
   TEST(test_parse_structure, required_fields_errors) {
     auto s = TEST_FCC_STRUCTURE<double>;
-    auto r1 = io::config::parse_structure_config<double>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", 4},
-    });
-    assert_holds_error<"species", CODE_NOT_FOUND>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-    });
-    assert_holds_error<"species", CODE_OUT_OF_RANGE>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", {1, 2, 3}},
-    });
+    json document = {{"structure",
+                      {
+                          {"lattice", s.lattice},
+                          {"coords", s.frac_coords},
+                          {"species", 4},
+                      }}};
+
+    auto assert_holds_error
+        = make_assert_holds_error(io::config::parse_structure_config<"structure", double>);
+
+    document["structure"].erase("species");
+    assert_holds_error("species", CODE_NOT_FOUND, document);
+    document["structure"]["species"] = {1, 2, 3};
+    assert_holds_error("species", CODE_OUT_OF_RANGE, document);
 
     // test invalid atomic speices
-    assert_holds_error<"species", CODE_OUT_OF_RANGE>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", {1, 2, 3, -5}},
-    });
+    document["structure"]["species"] = {1, 2, 3, -5};
+    assert_holds_error("species", CODE_OUT_OF_RANGE, document);
 
+    document["structure"]["species"] = {"Al", "Mg", "Si", "??"};
     // test invalid atomic species
-    assert_holds_error<"species", CODE_OUT_OF_RANGE>({
-        {"lattice", s.lattice},
-        {"coords", s.frac_coords},
-        {"species", {"Al", "Mg", "Si", "??"}},
-    });
+    assert_holds_error("species", CODE_OUT_OF_RANGE, document);
 
+    document["structure"]["species"] = {"Al", "Mg", "Si", "Ge"};
+    document["structure"]["coords"] = std::vector{1, 2, 3, 4};
     // test wrong shape for coords
-    assert_holds_error<"coords", CODE_TYPE_ERROR>({
-        {"lattice", s.lattice},
-        {"coords", std::vector{1, 2, 3, 4}},
-        {"species", {1, 2, 3}},
-    });
+    assert_holds_error("coords", CODE_TYPE_ERROR, document);
 
     // test wrong shape for lattice
-    assert_holds_error<"lattice", CODE_OUT_OF_RANGE>({
-        {"lattice", s.frac_coords},
-        {"coords", s.lattice},
-        {"species", {1, 2, 3}},
-    });
+    document["structure"]["lattice"] = s.frac_coords;
+    document["structure"]["coords"] = s.lattice;
+    assert_holds_error("lattice", CODE_OUT_OF_RANGE, document);
+  }
+
+  TEST(test_parse_composition, empty) {
+    auto s = TEST_FCC_STRUCTURE<double>;
+    json document{{"lattice", s.lattice}, {"coords", s.frac_coords}, {"species", s.species}};
+    auto key = "composition";
+    document[key] = json::array();
+
   }
 
   /*TEST(test_parse_structure, which) {
