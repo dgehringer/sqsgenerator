bad0206aa97ff597248307a58a5d34231eacb639
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-12-14 22:16:03 +0100

    ♻️ refactor imports

--- include/sqsgen/io/parsing.h
+++ include/sqsgen/io/parsing.h
@@ -8,6 +8,7 @@
 #include <optional>
 #include <string>
 
+#include "sqsgen/io/json.h"
 #include "sqsgen/core/helpers.h"
 
 namespace sqsgen::io {
@@ -205,93 +206,9 @@ namespace sqsgen::io {
     return get_either<key, As>(json);
   }
 
-  template <class... Args> using parse_result_t = std::variant<parse_error, Args...>;
-
-  template <class... Args> bool holds_result(parse_result_t<Args...> const& a) {
-    return !std::holds_alternative<parse_error>(a);
-  }
-
-  template <class... Args> bool holds_error(parse_result_t<Args...> const& a) {
-    return std::holds_alternative<parse_error>(a);
-  }
-
-  template <class A> A get_result(parse_result_t<A> const& a) { return std::get<A>(a); }
-
-  template <class A, class Fn>
-  parse_result_t<std::optional<A>> validate(std::optional<parse_result_t<A>>&& a, Fn&& fn) {
-    if (a.has_value()) {
-      if (holds_result(a.value())) {
-        auto result = fn(std::forward<std::decay_t<A>>(get_result(a.value())));
-        if (holds_result(result)) return std::make_optional(get_result(result));
-        return std::get<parse_error>(result);
-      }
-      return std::get<parse_error>(a.value());
-    }
-    return std::nullopt;
-  }
-
-  template <class A, class Fn>
-  parse_result_t<A> validate(std::optional<parse_result_t<A>>&& a, A&& default_value, Fn&& fn) {
-    if (a.has_value()) {
-      if (holds_result(a.value())) {
-        auto result = fn(std::forward<std::decay_t<A>>(get_result(a.value())));
-        if (holds_result(result)) return get_result(result);
-        return std::get<parse_error>(result);
-      }
-      return std::get<parse_error>(a.value());
-    }
-    return default_value;
-  }
-
-  template <class Collapse, class... Args, class... Fn>
-  parse_result_t<Collapse> validate(std::optional<parse_result_t<Args...>>&& a,
-                                    Collapse&& default_value, Fn&&... fn) {
-    if (a.has_value()) {
-      if (holds_result(a.value())) {
-        auto error_case = [](parse_error&& error) -> parse_result_t<Collapse> { return error; };
-        auto result = std::visit(overloaded{error_case, fn...},
-                                 std::forward<parse_result_t<Args...>>(a.value()));
-        if (holds_result(result)) return get_result(result);
-        return std::get<parse_error>(result);
-      }
-      return std::get<parse_error>(a.value());
-    }
-    return default_value;
-  }
-  template <class Collapse, class... Args, class... Fn>
-  parse_result_t<Collapse> validate(parse_result_t<Args...>&& a, Fn&&... fn) {
-    if (holds_result(a)) {
-      auto error_case = [](parse_error&& error) -> parse_result_t<Collapse> { return error; };
-      return std::visit(overloaded{error_case, fn...}, std::forward<parse_result_t<Args...>>(a));
-    }
-    return std::get<parse_error>(a);
-  }
-
-  template <class... Options, class Option>
-  parse_result_t<Options...> forward_variant(std::variant<parse_error, Option>&& opt) {
-    if (std::holds_alternative<parse_error>(opt)) {
-      return std::get<parse_error>(opt);
-    }
-    return std::get<Option>(opt);
-  }
-
-  template <string_literal key, class Value>
-  std::optional<parse_result_t<Value>> get_optional(nlohmann::json const& json) {
+  template <string_literal key, class Value, class Document>
+  std::optional<parse_result<Value>> get_optional(Document const& json) {
     return get_either_optional<key, Value>(json);
-  }
-
-  template <class... Args>
-  parse_result_t<std::tuple<Args...>> combine(parse_result_t<Args>&&... args) {
-    std::optional<parse_error> error = std::nullopt;
-    ((error = error.has_value()
-                  ? error
-                  : (holds_error<Args>(args) ? std::make_optional(std::get<parse_error>(args))
-                                             : std::nullopt)),
-     ...);
-    if (error.has_value()) {
-      return error.value();
-    }
-    return std::make_tuple(std::get<Args>(args)...);
   }
 
 }  // namespace sqsgen::io
