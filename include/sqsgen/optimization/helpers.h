caca903ce40dabc34f8656a29f7c59d9fef70936
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-01-09 10:31:05 +0100

    🎨 factor out count_bonds and compute_objective

--- include/sqsgen/optimization/helpers.h
+++ include/sqsgen/optimization/helpers.h
@@ -14,9 +14,36 @@ namespace sqsgen::optimization::helpers {
 
   namespace detail {}
 
-  template <class T> cube_t<T> scaled_pair_weights(cube_t<T> const& pair_weights, shell_weights_t<T> const& weights, usize_t num_species) {
+  enum class ScopedExecutionMode : uint8_t {
+    SCOPED_EXECUTION_MODE_INIT,
+    SCOPED_EXECUTION_MODE_DESTRUCT,
+    SCOPED_EXECUTION_MODE_BOTH,
+  };
+
+  template <class Fn> struct scoped_execution {
+    ScopedExecutionMode mode;
+    Fn fn;
+
+    explicit scoped_execution(Fn&& fn, ScopedExecutionMode mode
+                                       = ScopedExecutionMode::SCOPED_EXECUTION_MODE_BOTH)
+        : mode(mode), fn(fn) {
+      if (mode == ScopedExecutionMode::SCOPED_EXECUTION_MODE_BOTH
+          || mode == ScopedExecutionMode::SCOPED_EXECUTION_MODE_INIT)
+        fn();
+    }
+    ~scoped_execution() {
+      if (mode == ScopedExecutionMode::SCOPED_EXECUTION_MODE_BOTH
+          || mode == ScopedExecutionMode::SCOPED_EXECUTION_MODE_DESTRUCT)
+        fn();
+    }
+  };
+
+  template <class T> cube_t<T> scaled_pair_weights(cube_t<T> const& pair_weights,
+                                                   shell_weights_t<T> const& weights,
+                                                   usize_t num_species) {
     auto w(pair_weights);
-    auto shells_rmap = std::get<1>(core::helpers::make_index_mapping<usize_t>(weights | views::elements<0>));
+    auto shells_rmap
+        = std::get<1>(core::helpers::make_index_mapping<usize_t>(weights | views::elements<0>));
     for (auto s = 0; s < weights.size(); ++s) {
       auto w_s = weights.at(shells_rmap.at(s));
       for (auto xi = 0; xi < num_species; ++xi)
@@ -66,6 +93,34 @@ namespace sqsgen::optimization::helpers {
     }
     assert(bounds.size() == composition.size());
     return std::make_tuple(sorted, bounds, sort_order);
+  }
+
+  void count_bonds(cube_t<usize_t>& bonds, auto const& pairs, configuration_t const& species) {
+    bonds.setConstant(0);
+    for (auto& [i, j, s] : pairs) {
+      auto si{species[i]};
+      auto sj{species[j]};
+      ++bonds(s, si, sj);
+    }
+  }
+
+  template <class T> T compute_objective(cube_t<T>& sro, cube_t<usize_t> const& bonds,
+                                         cube_t<T> const& prefactors, cube_t<T> const& pair_weights,
+                                         cube_t<T> const& target, auto num_shells,
+                                         auto num_species) {
+    sro.setConstant(T(0.0));
+    T objective{0.0};
+    for (auto s = 0; s < num_shells; ++s)
+      for (auto xi = 0; xi < num_species; ++xi)
+        for (auto eta = xi; eta < num_species; ++eta) {
+          auto pair_bonds = bonds(s, xi, eta) + bonds(s, eta, xi);
+          T sigma_s_xi_eta = T(1.0) - static_cast<T>(pair_bonds) * prefactors(s, xi, eta);
+          sro(s, xi, eta) = sigma_s_xi_eta;
+          sro(s, eta, xi) = sigma_s_xi_eta;
+          objective += pair_weights(s, xi, eta) * core::helpers::absolute(sigma_s_xi_eta - target(s, xi, eta));
+        }
+
+    return objective;
   }
 
 }  // namespace sqsgen::optimization::helpers
