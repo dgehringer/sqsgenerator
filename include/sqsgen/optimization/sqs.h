e4e6c5c0c0ffb832d77c7205c27aee472493fd96
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-01-11 14:41:46 +0100

    finish MPI implementation

--- include/sqsgen/optimization/sqs.h
+++ include/sqsgen/optimization/sqs.h
@@ -8,27 +8,12 @@
 #include "sqsgen/config.h"
 #include "sqsgen/core/shuffle.h"
 #include "sqsgen/core/thread_pool.h"
-#include "sqsgen/io/mpi.h"
+#include "sqsgen/optimization/collection.h"
+#include "sqsgen/optimization/comm.h"
 #include "sqsgen/optimization/helpers.h"
 #include "sqsgen/types.h"
 
 namespace sqsgen::optimization {
-
-  template <class, SublatticeMode> struct sqs_result {};
-
-  template <class T> struct sqs_result<T, SUBLATTICE_MODE_INTERACT> {
-    std::optional<rank_t> rank;
-    T objective;
-    configuration_t species;
-    cube_t<T> parameters;
-  };
-
-  template <class T> struct sqs_result<T, SUBLATTICE_MODE_SPLIT> {
-    std::optional<std::vector<rank_t>> rank;
-    std::vector<T> objective;
-    std::vector<configuration_t> species;
-    std::vector<cube_t<T>> parameters;
-  };
 
   template <class, IterationMode, SublatticeMode> class optimizer {};
 
@@ -39,13 +24,21 @@ namespace sqsgen::optimization {
     std::atomic<iterations_t> _working;
     std::atomic<T> _best_objective;
 
+    void update_objective(comm::MPICommunicator<T>& comm, T objective) {
+      if (objective < _best_objective.load()) {
+        _best_objective.exchange(objective);
+        // we broadcast it to the other ranks, if our configuration is truly the best
+        comm.broadcast_objective(T(objective));
+      }
+    }
+
   public:
     explicit optimizer(configuration<T> config)
         : config(config), _best_objective(std::numeric_limits<T>::max()) {}
 
     void run() {
       using namespace sqsgen::core::helpers;
-      auto comm = io::MPICommunicator<T>(config.thread_config);
+      auto comm = comm::MPICommunicator<T>(config.thread_config);
 
       auto structure = config.structure.structure().apply_composition(config.composition);
       /* sort the structure by the sites sublattice index
@@ -77,76 +70,77 @@ namespace sqsgen::optimization {
       });
       core::shuffler shuffling(bounds);
 
+      auto head = comm.is_head();
       iterations_t chunk_size = config.chunk_size;
       iterations_t iterations = config.iterations.value();
-      auto pool = core::thread_pool<core::Task<iterations_t>>(comm.num_threads());
+      sqs_result_collection<T, SUBLATTICE_MODE_INTERACT> results{};
+      core::thread_pool<core::Task<iterations_t>> pool(comm.num_threads());
 
-      std::stop_token st = pool.get_stop_token();
-      std::function<void(iterations_t)> worker =
-          [this, &pool, &species_packed, &shuffling, &worker, &comm, st, pairs,
-           num_shells = weights.size(), num_species = sorted.num_species, total = iterations,
-           chunk_size, prefactors, target, pair_weights](iterations_t iterations) {
-            _working.fetch_add(iterations);
-            configuration_t species(species_packed);
-            cube_t<usize_t> bonds(num_shells, num_species, num_species);
-            cube_t<T> sro(num_shells, num_species, num_species);
-            T objective(0);
-            for (iterations_t i = 0; i < iterations; ++i) {
-              if (st.stop_requested()) return;
-              bonds.setConstant(0);
-              shuffling.shuffle(species);
-              for (auto& [i, j, s] : pairs) {
-                auto si{species[i]};
-                auto sj{species[j]};
-                ++bonds(s, si, sj);
-              }
-              // symmetrize bonds for each shell and compute objective function
-              sro.setConstant(T(0.0));
-              objective = 0.0;
-              for (auto s = 0; s < num_shells; ++s) {
-                for (auto xi = 0; xi < num_species; ++xi) {
-                  for (auto eta = xi; eta < num_species; ++eta) {
-                    auto pair_bonds = bonds(s, xi, eta) + bonds(s, eta, xi);
-                    T sigma_s_xi_eta = T(1.0) - static_cast<T>(pair_bonds) * prefactors(s, xi, eta);
-                    sro(s, xi, eta) = sigma_s_xi_eta;
-                    sro(s, eta, xi) = sigma_s_xi_eta;
-                    objective
-                        += pair_weights(s, xi, eta) * absolute(sigma_s_xi_eta - target(s, xi, eta));
-                  }
-                }
-              }
-              if (objective <= _best_objective.load()) {
-                // pull in changes from other ranks. Has another rank found a better configuration?
-                std::cout << comm.rank() << ", " << _best_objective.load() << std::endl;
-                comm.synchronize_objective(_best_objective);
-                std::cout << "\t" << comm.rank() << ", " << _best_objective.load() << ", " << objective << std::endl;
-                // load again - a different thread may also have found a better configuration
-                if (objective < _best_objective.load()) {
-                  _best_objective.exchange(objective);
-                  // we broadcast it to the other ranks, if our configuration is truly the best
-                  comm.broadcast_objective(T(objective));
-                }
-
-              }
-            }
-            _working.fetch_add(-iterations);
-            comm.synchronize_objective(_best_objective);
-            iterations_t finished = _finished.fetch_add(iterations);
-            if (finished >= total)
-              pool.stop();
-            else {
+      const auto schedule_chunk
+          = [this, &pool, chunk_size, total = iterations](auto fn, iterations_t finished) {
               iterations_t working = _working.load();
               iterations_t next_block = std::min(total - finished - working, chunk_size);
-              if (_scheduled.load() + working < total) {
-                pool.enqueue_fn(worker, iterations_t(next_block));
+              if (_scheduled.load() + working <= total) {
+                pool.enqueue_fn(fn, iterations_t(next_block));
                 _scheduled.fetch_add(next_block);
               }
+            };
+
+      const auto pull_results
+          = [&, head]() {
+            auto buffer = sqs_result<T, SUBLATTICE_MODE_INTERACT>::empty(weights, sorted);
+              if (head)
+                for (auto&& r : comm.pull_results(buffer)) results.insert_result(std::move(r));
+            };
+
+      std::stop_token st = pool.get_stop_token();
+      std::function<void(iterations_t)> worker = [this, &pool, &species_packed, &shuffling, &worker,
+                                                  &comm, &schedule_chunk, &results, &pull_results,
+                                                  st, pairs, num_shells = weights.size(),
+                                                  num_species = sorted.num_species,
+                                                  total = iterations, prefactors, target,
+                                                  pair_weights, head](iterations_t iterations) {
+        _working.fetch_add(iterations);
+        configuration_t species(species_packed);
+        cube_t<usize_t> bonds(num_shells, num_species, num_species);
+        cube_t<T> sro(num_shells, num_species, num_species);
+        sqs_result<T, SUBLATTICE_MODE_INTERACT> buffer{
+            std::nullopt, std::numeric_limits<T>::infinity(), species, sro};
+        helpers::scoped_execution([&] { comm.pull_objective(_best_objective); });
+        helpers::scoped_execution([&] { pull_results(); });
+        for (iterations_t i = 0; i < iterations; ++i) {
+          if (st.stop_requested()) return;
+          shuffling.shuffle(species);
+          helpers::count_bonds(bonds, pairs, species);
+          // symmetrize bonds for each shell and compute objective function
+          T objective = helpers::compute_objective(sro, bonds, prefactors, pair_weights, target,
+                                                   num_shells, num_species);
+          if (objective <= _best_objective.load()) {
+            // pull in changes from other ranks. Has another rank found a better
+            comm.pull_objective(_best_objective);
+            update_objective(comm, objective);
+            sqs_result<T, SUBLATTICE_MODE_INTERACT> current{std::nullopt, objective, species, sro};
+            if (!head)
+              comm.send_result(std::move(current));
+            else {
+              // this sqs_result_collection::insert is thread safe
+              results.insert_result(std::forward<decltype(current)>(current));
+              pull_results();
             }
-          };
+          }
+        }
+        _working.fetch_add(-iterations);
+        iterations_t finished = _finished.fetch_add(iterations);
+        if (finished >= total)
+          pool.stop();
+        else
+          schedule_chunk(worker, iterations_t(finished));
+      };
       pool.start();
-      for (auto n = 0; n <= std::thread::hardware_concurrency(); ++n)
-        pool.enqueue_fn(worker, iterations_t(chunk_size));
+      for_each([&worker, &schedule_chunk](auto) { schedule_chunk(worker, 0); }, comm.num_threads());
       pool.join();
+      std::cout << comm.rank() << "{" << head << "}" << " -> " << results.num_results()
+                << std::endl;
     }
 
   private:
