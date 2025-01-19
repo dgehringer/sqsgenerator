ecd972cb60530c2acd53648d84ccbac3af02ce2f
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-01-19 21:28:50 +0100

    🎨 improve sublattice mode parsing

--- include/sqsgen/io/config/combined.h
+++ include/sqsgen/io/config/combined.h
@@ -18,21 +18,9 @@ namespace sqsgen::io::config {
   static constexpr iterations_t chunk_size_default = 100000;
 
   template <string_literal key, class Document>
-  parse_result<IterationMode> parse_mode(Document const& document) {
-    using result_t = parse_result<IterationMode>;
-    return get_optional<key, IterationMode>(document)
-        .value_or(parse_result<IterationMode>{ITERATION_MODE_RANDOM})
-        .and_then([&](auto&& mode) -> result_t {
-          return parse_for_mode<key>(
-              [&]() -> result_t { return result_t{mode}; },
-              [&]() -> result_t {
-                if (mode == ITERATION_MODE_SYSTEMATIC)
-                  return parse_error::from_msg<key, CODE_BAD_VALUE>(
-                      "It is not possible to do an exhaustive search on multiple sublattices");
-                return result_t{mode};
-              },
-              document);
-        });
+  parse_result<IterationMode> parse_iteration_mode(Document const& document) {
+    return get_optional<key, IterationMode>(document).value_or(
+        parse_result<IterationMode>{ITERATION_MODE_RANDOM});
   }
 
   template <string_literal key, class Document>
@@ -69,30 +57,52 @@ namespace sqsgen::io::config {
         });
   }
 
+  template <string_literal key, class Document>
+  parse_result<SublatticeMode> parse_sublattice_mode(Document const& doc) {
+    return get_optional<"sublattice_mode", SublatticeMode>(doc)
+        .value_or(parse_result<SublatticeMode>{SUBLATTICE_MODE_INTERACT})
+        .and_then([&](auto&& mode) -> parse_result<SublatticeMode> {
+          if (mode == SUBLATTICE_MODE_INVALID)
+            return parse_error::from_msg<key, CODE_BAD_VALUE>(
+                R"(Invalid sublattice mode. Must be either "interact" or "split")");
+          return mode;
+        });
+  }
+
   template <class T, class Document>
   parse_result<configuration<T>> parse_config(Document const& doc) {
     return config::parse_structure_config<"structure", T>(doc)
-        .combine(parse_mode<"mode">(doc))
+        .combine(parse_iteration_mode<"mode">(doc))
+        .combine(parse_sublattice_mode<"sublattice_mode">(doc))
         .and_then([&](auto&& sc_and_m) {
-          auto [sc, mode] = sc_and_m;
+          auto [sc, iteration_mode, sublattice_mode] = sc_and_m;
           auto structure = sc.structure();
+          /*if (iteration_mode == ITERATION_MODE_SYSTEMATIC
+              && sublattice_mode == SUBLATTICE_MODE_SPLIT)
+            return parse_error::from_msg<"mode", CODE_BAD_VALUE>(
+                "It is not possible to do an exhaustive search on multiple sublattices");*/
           return config::parse_composition<"composition", "sites">(doc, structure.species)
-              .combine(parse_iterations<"iterations">(doc, mode))
+              .combine(parse_iterations<"iterations">(doc, iteration_mode))
               .and_then([&](auto&& comp_and_iter) {
                 auto [composition, iterations] = comp_and_iter;
                 return config::parse_shell_radii<"shell_radii">(
-                           doc, std::forward<decltype(structure)>(structure), composition)
+                           doc, sublattice_mode, std::forward<decltype(structure)>(structure),
+                           composition)
                     .and_then([&](auto&& radii) {
-                      return config::parse_shell_weights<"shell_weights", T>(doc, radii)
+                      return config::parse_shell_weights<"shell_weights", T>(doc, sublattice_mode,
+                                                                             radii)
                           .and_then([&](auto&& weights) {
                             return config::parse_prefactors<"prefactors", T>(
-                                       doc, std::forward<decltype(structure)>(structure),
-                                       composition, radii, weights)
+                                       doc, sublattice_mode,
+                                       std::forward<decltype(structure)>(structure), composition,
+                                       radii, weights)
                                 .combine(config::parse_pair_weights<"pair_weights", T>(
-                                    doc, std::forward<decltype(structure)>(structure), composition,
+                                    doc, sublattice_mode,
+                                    std::forward<decltype(structure)>(structure), composition,
                                     weights))
                                 .combine(config::parse_target_objective<"target_objective", T>(
-                                    doc, std::forward<decltype(structure)>(structure), composition,
+                                    doc, sublattice_mode,
+                                    std::forward<decltype(structure)>(structure), composition,
                                     weights))
                                 .combine(parse_chunk_size<"chunk_size">(doc, iterations))
                                 .combine(parse_threads_per_rank<"threads_per_rank">(doc))
@@ -101,6 +111,8 @@ namespace sqsgen::io::config {
                                         thread_config]
                                       = arrays;
                                   return configuration<T>{
+                                      sublattice_mode,
+                                      iteration_mode,
                                       std::forward<structure_config<T>>(sc),
                                       std::forward<std::vector<sublattice>>(composition),
                                       std::forward<stl_matrix_t<T>>(radii),
@@ -108,7 +120,6 @@ namespace sqsgen::io::config {
                                       std::forward<std::vector<cube_t<T>>>(prefactors),
                                       std::forward<std::vector<cube_t<T>>>(pair_weights),
                                       std::forward<std::vector<cube_t<T>>>(target_objective),
-                                      mode,
                                       iterations,
                                       chunk_size,
                                       thread_config};
