0b77ad0f0ab7d8299d59e7ff089f5d64c6fbbae6
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-03-30 15:34:49 +0200

    ✅ unit test fix

--- python/tests/test_core.py
+++ python/tests/test_core.py
@@ -52,31 +52,22 @@ def test_systematic_all_results(prec):
 
 @pytest.mark.parametrize("prec", [single, double])
 def test_systematic_all_results_partial(prec):
-    return
-    settings = dict(
-        prec=prec,
-        mode=systematic,
-        structure=dict(
-            lattice=np.diag([1, 2, 3]),
-            coords=np.array(
-                [[0.0, 0.0, 0.0], [0.5, 0.5, 0.0], [0.0, 0.5, 0.5], [0.5, 0.0, 0.5]]
-            ),
-            species=[1, 2] * 2,
-            supercell=[2, 2, 2],
+    settings = default_settings(prec)
+    settings["structure"] = dict(
+        lattice=np.diag([1, 2, 3]),
+        coords=np.array(
+            [[0.0, 0.0, 0.0], [0.5, 0.5, 0.0], [0.0, 0.5, 0.5], [0.5, 0.0, 0.5]]
         ),
-        pair_weights=0,
-        composition={"sites": "H", "Ti": 8, "Al": 8},
+        species=[1, 2] * 2,
+        supercell=[2, 2, 2],
     )
 
     config = parse_config(settings)
     results = optimize(config)
     assert results.num_results() == config.iterations
     solutions = [sol for _, rset in results for sol in rset]
-    ranks = set(range(1, config.iterations + 1))
     for sol in solutions:
         assert math.isclose(sol.objective, 0.0)
         assert collections.Counter(sol.structure().species) == collections.Counter(
             {22: 8, 13: 8, 2: 16}
         )
-        ranks.discard(int(sol.rank(10)))
-    assert not ranks
