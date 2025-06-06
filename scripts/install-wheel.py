a3416e63cfe0bc7d68cb2df5bea6674ee1cc3ec7
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-06-06 21:30:02 +0200

    👷 try install built wheel with python script

---
+++ scripts/install-wheel.py
@@ -0,0 +1,29 @@
+import platform
+import subprocess
+import sys
+import tempfile
+from pathlib import PurePosixPath, PureWindowsPath
+
+if __name__ == "__main__":
+    match platform.system():
+        case "Darwin":
+            tmp_dir = PurePosixPath(tempfile.gettempdir())
+            built_wheel_dir = tmp_dir / "cibw-run-*" / "cp3*macosx_*" / "built_wheel"
+        case "Linux":
+            temp_dir = PurePosixPath("/tmp/cibuildwheel")
+            built_wheel_dir = temp_dir / "built_wheel"
+        case "Windows":
+            tmp_dir = PureWindowsPath(tempfile.gettempdir())
+            built_wheel_dir = tmp_dir / "cibw-run-*" / "cp3*win_*" / "built_wheel"
+        case _:
+            raise RuntimeError(f"Unsupported platform: {platform.system()}")
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-mpip",
+            "install",
+            "-v",
+            built_wheel_dir / "sqsgenerator*.whl",
+        ]
+    )
