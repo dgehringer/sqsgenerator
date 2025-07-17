351eaa18441cd1b037aac7819e5491f05ddfc167
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-17 10:03:46 +0200

    ✨ update __all__ exports, enhance templates in CLI

--- python/sqsgenerator/__init__.py
+++ python/sqsgenerator/__init__.py
@@ -1,6 +1,8 @@
 from ._adapters import HAVE_ASE, HAVE_PYMATGEN, available_formats, read, write
 from ._optimize import optimize, parse_config
 
+__all__ = ["HAVE_ASE", "HAVE_PYMATGEN", "optimize", "parse_config"]
+
 if HAVE_PYMATGEN:
     from ._adapters import (
         from_pymatgen,
@@ -9,6 +11,14 @@ if HAVE_PYMATGEN:
         to_pymatgen,
         write_pymatgen,
     )
+
+    __all__ += [
+        "from_pymatgen",
+        "pymatgen_formats",
+        "read_pymatgen",
+        "to_pymatgen",
+        "write_pymatgen",
+    ]
 
 if HAVE_ASE:
     from ._adapters import (
@@ -19,4 +29,4 @@ if HAVE_ASE:
         write_ase,
     )
 
-__all__ = ["HAVE_ASE", "HAVE_PYMATGEN", "optimize", "parse_config"]
+    __all__ += ["ase_formats", "from_ase", "read_ase", "to_ase", "write_ase"]
