0c20c310c48bf2fd3819d8229eb109fd66d360b4
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-20 23:12:33 +0200

    move template generation to CI

--- scripts/build-cli-mpi.sh
+++ scripts/build-cli-mpi.sh
@@ -5,6 +5,7 @@ THIS_DIR=$(dirname "$(readlink -f "$0")")
 PROJECT_DIR="${THIS_DIR}/.."
 TOOLCHAIN_FILE="${PROJECT_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
+python ${THIS_DIR}/package-templates.py ${PROJECT_DIR}/templates
 
 cmake \
   -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" \
