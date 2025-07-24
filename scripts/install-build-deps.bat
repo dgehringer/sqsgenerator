fee40cb55f737ead0809874bcb0ce5448a5c6f6c
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-24 22:59:58 +0200

    💚 simplify & accelerate wheels

--- scripts/install-build-deps.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pip install --upgrade pip-tools
-pip-compile -v --all-build-deps --output-file=requirements-ci.txt --strip-extras python/pyproject.toml
-pip install -r requirements-ci.txt
