5137feb39620900f600908e1ac6dbc0a50491bc2
dgehringer <35268573+dgehringer@users.noreply.github.com>
2026-07-27 14:46:33 +0200

    lint

---
+++ js/wasm/parser.cpp
@@ -0,0 +1,85 @@
+#include <emscripten/bind.h>
+#include <emscripten/emscripten.h>
+#include <emscripten/proxying.h>
+#include <emscripten/threading.h>
+
+#include "sqsgen/core/helpers.h"
+#include "sqsgen/core/results.h"
+#include "sqsgen/io/config/combined.h"
+#include "sqsgen/io/json.h"
+#include "sqsgen/io/parsing.h"
+#include "sqsgen/io/structure.h"
+#include "sqsgen/log.h"
+#include "sqsgen/types.h"
+
+using namespace emscripten;
+using namespace sqsgen::core::helpers;
+using namespace sqsgen::core;
+
+template <string_literal Name, class T>
+  requires std::is_arithmetic_v<T>
+constexpr auto format_prec() {
+  if constexpr (std::is_same_v<double, T>)
+    return (Name + string_literal("Double"));
+  else if constexpr (std::is_same_v<float, T>)
+    return (Name + string_literal("Float"));
+};
+
+template <class T> nlohmann::json to_json(T&& value) {
+  nlohmann::json j = value;
+  return j;
+}
+
+template <class T> T from_json(nlohmann::json const& j) { return j.get<T>(); }
+
+nlohmann::json to_internal(val const& v) {
+  return nlohmann::json::parse(val::global("JSON").call<std::string>("stringify", v));
+}
+
+val to_js(nlohmann::json const& j) {
+  return val::global("JSON").call<val, std::string>("parse", j.dump());
+}
+
+val error_to_js(sqsgen::io::parse_error const& e) {
+  return to_js(nlohmann::json{
+      {"key", e.key},
+      {"msg", e.msg},
+      {"code", static_cast<int>(e.code)},
+      {"parameter", e.parameter},
+  });
+}
+
+val parse_result_to_js(sqsgen::io::parse_result<nlohmann::json> const& r) {
+  return r.failed() ? error_to_js(r.error()) : to_js(r.result());
+}
+
+val parse_config(val const& v) {
+  using namespace sqsgen;
+  using namespace sqsgen::io;
+  using namespace sqsgen::io::config;
+
+  nlohmann::json json = to_internal(v);
+  return parse_result_to_js(parse_config(json).collapse<nlohmann::json>(
+      [](auto&& config) -> parse_result<nlohmann::json> { return {to_json(config)}; }));
+}
+
+EMSCRIPTEN_BINDINGS(m) {
+  // Core optimization function
+  register_optional<val>();
+  register_optional<std::string>();
+  register_optional<std::size_t>();
+  register_optional<std::uint64_t>();
+  register_optional<double>();
+
+  enum_<sqsgen::Prec>("Prec")
+      .value("single", sqsgen::Prec::PREC_SINGLE)
+      .value("double", sqsgen::Prec::PREC_DOUBLE);
+
+  // Helper functions
+  function("parseConfig", &parse_config);
+}
+
+int main() {
+  emscripten_exit_with_live_runtime();
+  return EXIT_SUCCESS;
+}
